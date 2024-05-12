7ad690d50efd7681306ccd4077542fd42ad37818
Saim <admin@gmail.com>
2024-05-12 09:30:04 +0600

    added basic middlwares

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
-
 const app = express();
+const cors = require('cors');
+const helmet = require('helmet');
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
 
 
 
