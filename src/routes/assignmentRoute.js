69b2511f89df4450f772be714307a23105368555
Saim <admin@gmail.com>
2024-05-18 20:44:15 +0600

    remove authenticate middleware from assignment route

--- src/routes/assignmentRoute.js
+++ src/routes/assignmentRoute.js
@@ -10,9 +10,14 @@ const router = require('express').Router();
 const{authenticate} = require('../middlewares/authenticate');
 
 router.get('/', getAllAssignments);
-router.post('/',authenticate, createAssignment);
+router.post('/',createAssignment);
 router.get('/:id', getAssignmentById);
-router.put('/:id',authenticate, updateAssignment);
-router.delete('/:id',authenticate ,deleteAssignment);
+router.put('/:id',updateAssignment);
+router.delete('/:id',deleteAssignment);
+// router.get('/', getAllAssignments);
+// router.post('/',authenticate, createAssignment);
+// router.get('/:id', getAssignmentById);
+// router.put('/:id',authenticate, updateAssignment);
+// router.delete('/:id',authenticate ,deleteAssignment);
 
 module.exports = router;
